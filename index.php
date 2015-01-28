c794e0b49a817be5db74daad14ee8b6627971d91
Jacob Green <jacobcgreen@verizon.net>
2015-01-28 14:03:36 -0800

    finished putting other languages in blog

--- index.php
+++ index.php
@@ -7,9 +7,26 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+         <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/themes/smoothness/jquery-ui.css" />
+         <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js"></script>
         <title></title>
     </head>
     <body>
+        </div>
+      <div class="container">
+          <p>container</p>
+          <button><a href="index.html">Home</a></button>
+          <a href="projects.html">Projects</a>
+          <a href="technology.html">Technologies</a>
+          <a href="jquery.html">JQuery</a>
+            <div class="row">      
+                <p>row</p>
+                <div class="col-xs-1"></div>
+                <div class="col-xs-11"></div>
+            </div>
+        </div>
         <?php
         echo "Web srever is running locally";
         ?>
