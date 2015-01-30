816d6af4917acd6d50f28c2fbcf8ffe75320b590
Jacob Green <jacobcgreen@verizon.net>
2015-01-29 19:43:16 -0800

    Added die code

--- controller/create-db.php
+++ controller/create-db.php
@@ -1,4 +1,13 @@
 <?php
+
 require_once("../model/database.php");
 
 $connection = new mysqli($host, $username, $password);
+
+if($connection->connect_error){
+    die();
+}
+
+else{
+    echo "Success";
+}
--- index.php
+++ index.php
@@ -1,34 +1,28 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html>
     <head>
         <meta charset="UTF-8">
         <link rel="stylesheet" type="text/css" href="css/style.css">
         <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-         <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/themes/smoothness/jquery-ui.css" />
-         <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js"></script>
-        <title></title>
+        <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/themes/smoothness/jquery-ui.css" />
+        <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js"></script>
+        <title>Blog</title>
     </head>
     <body>
+    </div>
+    <div class="container">
+        <p>container</p>
+        <a href="" class="btn btn-info" role="button">Blog</a>
+        <a href="" class="btn btn-info" role="button"></a>
+        <a href="" class="btn btn-info" role="button"></a>
+        <a href="" class="btn btn-info" role="button"></a>
+        <div class="row">      
+            <p>row</p>
+            <div class="col-xs-1"></div>
+            <div class="col-xs-11"></div>
         </div>
-      <div class="container">
-          <p>container</p>
-          <button><a href="index.html">Home</a></button>
-          <a href="projects.html">Projects</a>
-          <a href="technology.html">Technologies</a>
-          <a href="jquery.html">JQuery</a>
-            <div class="row">      
-                <p>row</p>
-                <div class="col-xs-1"></div>
-                <div class="col-xs-11"></div>
-            </div>
-        </div>
-        <?php
-        echo "Web srever is running locally";
-        ?>
-    </body>
+    </div>
+    <?php
+    echo "Web srever is running locally";
+    ?>
+</body>
 </html>
