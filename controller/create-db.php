448532af77b7189afa8182bd3c722a2fac3fa1de
Jacob Green <jacobcgreen@verizon.net>
2015-02-06 14:00:29 -0800

    Added post

--- controller/create-db.php
+++ controller/create-db.php
@@ -1,11 +1,9 @@
 <?php
-
 require_once("../model/database.php");
 
 $connection = new mysqli($host, $username, $password);
 
 if ($connection->connect_error) {
-    die("error");
-} else {
-    echo "Success";
-}
+    die("<p>Error: " , $connection->connect_error . "</p>");
+} 
+
