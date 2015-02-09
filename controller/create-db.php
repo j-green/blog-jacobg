1978c3d90619f111810729a0d4c3f11dccc22eb9
Jacob Green <jacobcgreen@verizon.net>
2015-02-09 13:43:03 -0800

    yay

--- controller/create-db.php
+++ controller/create-db.php
@@ -4,6 +4,21 @@ require_once("../model/database.php");
 $connection = new mysqli($host, $username, $password);
 
 if ($connection->connect_error) {
-    die("<p>Error: " , $connection->connect_error . "</p>");
+    die("<p>Error: " . $connection->connect_error . "</p>");
 } 
 
+$exists = $connection->select_db($database);
+
+if(!$exists) {
+    $query = $connection->query("CREATE DATABASE $database");
+    
+if($query) {
+    echo "<p>Successfully created database:  ". $database . "</p>";
+    }
+}
+
+
+else{
+    echo "<p>Database already .</p>";
+}
+//$query = $connection->query
